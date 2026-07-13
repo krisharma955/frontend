941894d18a9a9b5e2663a7a38afaa0631f5fa5a9
krishsharma955 <krishsharma9005@gmail.com>
2026-07-13 12:15:36 +0530

    components

---
+++ react/react-components/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, corporis.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
