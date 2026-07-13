eb3a6456a16a9314ce1649d2e943c0b2a40d74af
krishsharma955 <krishsharma9005@gmail.com>
2026-07-13 11:42:29 +0530

    app structure

--- react/react-01/src/App.jsx
+++ react/react-01/src/App.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <div id = "101">
+      </div>
+      
+      <div id = "102">
+
+      </div>
+    </>
   )
 }
 
